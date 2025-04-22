49f3fd9b08b0379876f80ebd8b5c561c40e0bcb9
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-22 16:05:01 +0200

    day 7 learn flutter

--- hellow/lib/main.dart
+++ hellow/lib/main.dart
@@ -1,139 +1,155 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BusniessCardApp());
+  runApp(BusinessCardApp());
 }
 
-class BusniessCardApp extends StatelessWidget {
+class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 23, 30, 67),
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'Business Card',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 233, 234, 237),
-              ),
-            ),
-          ),
-          backgroundColor: Color.fromARGB(255, 23, 30, 67),
-        ),
-        //the application body
-        //print the appbar in the center of the screen
-        //the appbar is the top of the screen
+        backgroundColor: Color.fromARGB(122, 33, 33, 81),
         body: Column(
           children: [
             Center(
-              child: Column(
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          // ignore: sort_child_properties_last
-                          //   child: Container(
-                          //     margin: EdgeInsets.only(top: 150),
-                          //     width: 150,
-                          //     height: 150,
-                          //     decoration: BoxDecoration(
-                          //       color: Color.fromARGB(255, 233, 234, 237),
-                          //       borderRadius: BorderRadius.circular(150),
-                          //       image: DecorationImage(
-                          //         // Moved the image property here
-                          //         image: AssetImage('images/images.jpg'),
-                          //         fit: BoxFit.cover,
-                          //       ),
-                          //     ),
-                          //   ),
-                          //   borderRadius: BorderRadius.circular(150),
-                          // ),
-                          // ignore: prefer_const_constructors
-                          //the image  is in the center of the screen
-                          //use average radius to make the image round
-                          // ignore: prefer_const_constructors
-                          child: Center(
-                            child: Container(
-                              margin: EdgeInsets.only(top: 80),
-                              child: CircleAvatar(
-                                // ignore: prefer_const_constructors
-                                radius: 80,
-                                backgroundColor: Color.fromARGB(
-                                  255,
-                                  233,
-                                  234,
-                                  237,
-                                ),
-                                backgroundImage: AssetImage(
-                                  'images/images.jpg',
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        //the critical part of the code
-                        Text(
-                          'Ibrahim Elshishtawy',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'EduAUVICWANTArrows-VariableFont_wght',
-                            color: Color.fromARGB(255, 233, 234, 237),
-                          ),
-                        ),
-                        Text(
-                          'Flutter Developer',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 233, 234, 237),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Container(
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.phone_android,
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                ),
-                                SizedBox(width: 25, child: SizedBox(width: 25)),
-                                Center(
-                                  child: TextFormField(
-                                    initialValue: '+20 1223070571',
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: '+20 1223070571',
-                                      hintStyle: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0),
-                                      ),
-                                    ),
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromARGB(255, 0, 0, 0),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+              child: Container(
+                margin: EdgeInsets.only(top: 160),
+                child: CircleAvatar(
+                  radius: 120,
+
+                  backgroundColor: Color.fromARGB(255, 221, 221, 226),
+                  child: CircleAvatar(
+                    radius: 115,
+
+                    backgroundImage: AssetImage('images/me.JPG'),
                   ),
-                ],
+                ),
+              ),
+            ),
+            Text(
+              'Ibrahim Elshishtawy',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'EduAUVICWANTArrows-VariableFont_wght',
+                fontSize: 27,
+              ),
+            ),
+            Text(
+              'My Flutter Developer mobile',
+              style: TextStyle(color: Color.fromARGB(255, 223, 223, 228)),
+            ),
+            Divider(
+              color: Color.fromARGB(255, 250, 250, 251),
+              thickness: 2,
+              endIndent: 60,
+              indent: 60,
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 239, 241),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.phone_android,
+                      size: 32,
+                      color: Color.fromARGB(255, 34, 34, 80),
+                    ),
+                    Spacer(flex: 2),
+                    Text('(+20) 01223070571', style: TextStyle(fontSize: 23)),
+                    Spacer(flex: 2),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 239, 241),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.chat,
+                      size: 32,
+                      color: Color.fromARGB(255, 34, 34, 80),
+                    ),
+                    Spacer(flex: 2),
+                    Text('Hima Shishtawy', style: TextStyle(fontSize: 23)),
+                    Spacer(flex: 2),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 239, 241),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 32,
+                      color: Color.fromARGB(255, 34, 34, 80),
+                    ),
+                    Spacer(flex: 2),
+                    Text(
+                      'shishtawyhima@gmail.com',
+                      style: TextStyle(fontSize: 23),
+                    ),
+                    Spacer(flex: 2),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 239, 241),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                child: Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.facebook,
+                      size: 32,
+                      color: Color.fromARGB(255, 34, 34, 80),
+                    ),
+                    Spacer(flex: 2),
+                    Text('Ibrahim Elshishtawy', style: TextStyle(fontSize: 23)),
+                    Spacer(flex: 2),
+                  ],
+                ),
               ),
             ),
           ],
+          //this create image avatar
         ),
       ),
     );
